5a1c98d5e63d33d33aa4ccc030f89300cf76eb22
Marta Arcones <arcones@live.com>
2018-10-14 13:15:41 +0200

    Launch first instance

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "eu-west-3"
 }
+
+resource "aws_instance" "example" {
+  ami           = "ami-38a01045"
+  instance_type = "t2.nano"
+  tags {
+    Name = "terraform-example"
+  }
+}
