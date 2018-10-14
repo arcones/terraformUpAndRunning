0a5298280ce8722ab6253acfe5b86fc421af5fde
Marta Arcones <arcones@live.com>
2018-10-14 13:20:45 +0200

    Add script to instance

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-38a01045"
   instance_type = "t2.nano"
+  user_data     = <<-EOF
+                  #!/bin/bash
+                  echo "Hello arcones" > index.html
+                  nohup busybox httpd -f -p 8080 &
+                  EOF
   tags {
     Name = "terraform-example"
   }
