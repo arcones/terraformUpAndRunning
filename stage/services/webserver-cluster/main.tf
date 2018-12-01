2cc80aac77fa6d57cfd7f59798af16d9125e8e05
Marta Arcones <arcones@live.com>
2018-12-01 22:19:19 +0100

    Set script in a separate file

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -25,18 +25,22 @@ data "terraform_remote_state" "db" {
 
 ## EC2 AUTOSCALING GROUP CONFIGURATION
 
+data "template_file" "user_data" {
+  template = "${file("user-data.sh")}"
+
+  vars {
+    server_port = "${var.server_port}"
+    db_address  = "${data.terraform_remote_state.db.address}"
+    db_port     = "${data.terraform_remote_state.db.port}"
+  }
+}
+
 resource "aws_launch_configuration" "instances" {
   image_id        = "ami-0fad7824ed21125b1"
   instance_type   = "t2.nano"
   security_groups = ["${aws_security_group.security_group.id}"]
 
-  user_data = <<-EOF
-                  #!/bin/bash
-                  echo "Hello arcones" >> index.html
-                  echo "${data.terraform_remote_state.db.address}" >> index.html
-                  echo "${data.terraform_remote_state.db.port}" >> index.html
-                  nohup busybox httpd -f -p ${var.server_port} &
-                  EOF
+  user_data = "${data.template_file.user_data.rendered}"
 
   lifecycle {
     create_before_destroy = true
