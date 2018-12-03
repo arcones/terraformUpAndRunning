62c1c0c8aad9e8393de4431c924ae9eb61d43901
Marta Arcones <arcones@live.com>
2018-12-03 20:43:32 +0100

    Fix typo in script

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -36,7 +36,7 @@ module "webserver_cluster" {
   cluster_name           = "webservers-prod"
   db_remote_state_bucket = "teraform-up-and-running-arcones-state"
   db_remote_state_key    = "prod/services/data-stores/mysql/terraform.tfstate"
-  instance_type          = "t2.nano"
+  instance_type          = "t2.micro"
   min_size               = 2
   max_size               = 10
 }
